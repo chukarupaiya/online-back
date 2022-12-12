cae0c94c9597c00a326a75a101aca1abef130486
karupaiya chu <102406168+chukarupaiya@users.noreply.github.com>
2022-12-12 08:05:55 +0530

    Update config.js

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 exports.db = mysql.createConnection({
-  host: 'sql6.freesqatabase.com',
+  host: 'sql6.freesqldatabase.com',
   user:'sql6582494',
   password:'PSBI2zQEn7',
   database:'sql6582494',
